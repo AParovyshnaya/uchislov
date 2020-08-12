ec1ba7b30bc1efb6b4d14974f206954e200c79dd
AParovyshnaya <anastasya.parovyshnaya@gmail.com>
2020-08-12 10:20:26 +0300

    Отформатирован
    Отформатирован ParseTest.js. Тепрь его легче читать

--- src/main/resources/units/experements/ParseTest.js
+++ src/main/resources/units/experements/ParseTest.js
@@ -1,8 +1,86 @@
-function gp(lastNumber, fullTest) {
+document.addEventListener("DOMContentLoaded", showTask);
+function passageTest(test, fullTest) {
+    let button = document.getElementById("button");
+    button.onclick = function () {
+        checking(morsel, fullTest);
+        button.remove();
+    };
+}
+function parseTest(test) {
+    let target = document.getElementById("task-container");
+    let rightStaple = -1;
+    let morsel = 0;
+    let text;
+    while (rightStaple < test.lastIndexOf('>')) {
+        let leftStaple = test.indexOf(`<`, rightStaple);
+        text = test.substring(rightStaple + 1, leftStaple);
+        render(target, text, false);
+        rightStaple = test.indexOf(`>`, leftStaple);
+        text = test.substring(leftStaple + 1, rightStaple);
+        render(target, text, true);
+        morsel += 1;
+    }
+    text = test.substring(rightStaple + 1);
+    render(target, text, false);
+    return morsel;
+}
+function render(target, segment, isInput) {
+    let text;
+    if (isInput) {
+        text = document.createElement("input");
+        text.setAttribute("class", "orfo");
+        text.setAttribute("value", segment);
+        text.style.width = segment.length * 8 + 'px';
+    } else {
+        text = document.createElement("label");
+        text.setAttribute("class", "normal_words");
+        text.textContent = segment;
+    }
+    target.appendChild(text);
+}
+
+function generate(userContent, correctContent, trueOrFalse) {
+    let usersElement = document.getElementById("user_results");
+    let answersElement = document.getElementById("correctAnswers");
+    let disastrous;
+    let properly;
+    let correctAnswer;
+    if (trueOrFalse) {
+        disastrous = document.createElement("label");
+        disastrous.setAttribute("class", "disastrous");
+        disastrous.textContent = userContent;
+        correctAnswer = document.createElement("label");
+        correctAnswer.setAttribute("class", "properly");
+        correctAnswer.textContent = correctContent;
+    } else {
+        properly = document.createElement("label");
+        properly.setAttribute("class", "normal");
+        properly.textContent = userContent;
+        correctAnswer = document.createElement("label");
+        correctAnswer.setAttribute("class", "normal");
+        correctAnswer.textContent = correctContent;
+    }
+    usersElement.appendChild(disastrous);
+    answersElement.appendChild(correctAnswer);
+}
+
+function checking(lastNumber, fullTest) {
+    deleteTest();
+    takeData(lastNumber);
+    comparison(userTest, fullTest);
+    greeting(mistake);
+}
+
+function deleteTest() {
+    let target = document.getElementById("task-container");
+    target.remove();
+}
+
+function takeData(morsels) {
     let number = 0;
     let changed;
     let userTest = ``;
-    while (number < lastNumber) {
+    while (number < morsels) {
         changed = document.getElementsByClassName("normal_words")[number].textContent;
         userTest += changed;
         changed = document.getElementsByClassName("orfo")[number].value;
@@ -11,31 +89,34 @@ function gp(lastNumber, fullTest) {
     }
     changed = document.getElementsByClassName("normal_words")[number].textContent;
     userTest += changed;
+    return userTest;
+}
+function comparison(userTest, fullTest) {
     let index = 0;
     let mistake = 0;
     while (index < fullTest.length) {
-        let usersResult = document.getElementById("user_results");
-        let correctAnswer = document.getElementById("correctAnswers");
         let correctContent = fullTest.substring(index, index + 1);
         let userContent = userTest.substring(index, index + 1);
         if (userContent > correctContent) {
-            resultUser(usersResult, correctAnswer, userContent, correctContent, true);
+            generate(userContent, correctContent, true);
             mistake += 1;
         } else {
             if (userContent < correctContent) {
-                resultUser(usersResult, correctAnswer, userContent, correctContent, true);
+                generate(userContent, correctContent, true);
                 mistake += 1;
             } else {
                 if (correctContent == userContent) {
-                    resultUser(usersResult, correctAnswer, userContent, correctContent, false);
+                    generate(userContent, correctContent, false);
                 }
 
             }
         }
         index += 1;
     }
-    const target = document.getElementById("task-container");
-    target.remove();
+    return mistake;
+}
+
+function greeting(mistake) {
     if (mistake == 0) {
         let greeting = document.createElement("p");
         let plaseForGreeting = document.getElementById("greetingOrWishes");
@@ -43,78 +124,5 @@ function gp(lastNumber, fullTest) {
         greeting.textContent = "Молодец!Всё верно!";
         deleteResults.remove();
         plaseForGreeting.appendChild(greeting);
-    } 
-
-}
-
-function resultUser(elementForBirthUserResult, elementForCorrectAnswers, userContent, correctContent, trueOrFalse) {
-    let disastrousOrProperly;
-    let correctAnswer;
-    if (trueOrFalse) {
-        disastrousOrProperly = document.createElement("label");
-        disastrousOrProperly.setAttribute("class", "disastrous");
-        disastrousOrProperly.textContent = userContent;
-        correctAnswer = document.createElement("label");
-        correctAnswer.setAttribute("class", "properly");
-        correctAnswer.textContent = correctContent;
-
-
-    } else {
-        disastrousOrProperly = document.createElement("label");
-        disastrousOrProperly.setAttribute("class", "normal");
-        disastrousOrProperly.textContent = userContent;
-        correctAnswer = document.createElement("label");
-        correctAnswer.setAttribute("class", "normal");
-        correctAnswer.textContent = correctContent;
-
     }
-    elementForBirthUserResult.appendChild(disastrousOrProperly);
-    elementForCorrectAnswers.appendChild(correctAnswer);
-
-
 }
-
-    function render(target, segment, isInput) {
-        let label;
-        if (isInput) {
-            // Renders an input field
-            label = document.createElement("input");
-            label.setAttribute("class", "orfo");
-            label.setAttribute("value", segment);
-            label.style.width = segment.length*8 + 'px';
-        } else {
-            // Renders a plain text
-            label = document.createElement("label");
-            label.setAttribute("class", "normal_words");
-            label.textContent = segment;
-        }
-        // Appends created rendered segment to the given target
-        target.appendChild(label);
-    }
-
-    function showTask() {
-        const target = document.getElementById("task-container");
-        let test = "Аморальный <а>гр<о>ном в <А>встралии, <А>нтарктиде, Афр<и>ке и Азии строил а<л>юминиевые авт<о>к<о>ло<нн>ы рядом с а<лл>еями и акв<а>ториями, которые охраняли ав<и>ад<е>санты с <а>вт<о>мат<а>ми.";
-        let fullTest = "Аморальный агроном в Австралии, Антарктиде, Африке и Азии строил алюминиевые автоколонны рядом с аллеями и акваториями, которые охраняли авиадесанты с автоматами.";
-        let rightStaple = -1;
-        let morsel = 0;
-        let text;
-        while (rightStaple < test.lastIndexOf('>')) {
-            let leftStaple = test.indexOf(`<`, rightStaple);
-            text = test.substring(rightStaple + 1, leftStaple);
-            render(target, text, false);
-            rightStaple = test.indexOf(`>`, leftStaple);
-            text = test.substring(leftStaple + 1, rightStaple);
-            render(target, text, true);
-            morsel += 1;
-        }
-        text = test.substring(rightStaple + 1);
-        render(target, text, false);
-    let button = document.getElementById("button");
-    button.onclick = function () {
-        gp(morsel, fullTest);
-        button.remove();
-    };
-}
-
-document.addEventListener("DOMContentLoaded", showTask);
